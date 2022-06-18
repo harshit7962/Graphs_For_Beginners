290c0fd06d3e19ae3065e03ebd1787613884a9ab
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-18 09:49:37 +0530

    Create isCycle.cpp

---
+++ isCycle.cpp
@@ -0,0 +1,21 @@
+//Given a graph, we need to return if it contains cycle or not...
+
+bool checkForCycle(vector<int> adj[], vector<bool> &visited, int src, int parent) {
+    vis[src] = true;
+    for(int y:adj[src]) {
+        if(!vis[y]) {
+            if(checkForCycle(adj, vis, y, src)) return true;
+        } else if(y!=parent) return true;
+    }
+    
+    return false;
+}
+
+bool hasCycle(vector<int> adj[], int v) {
+    vector<bool> vis(v, false);
+    for(int i=0;i<v;i++){
+        if(!vis[v]) if(checkForCycle(adj, vis, i, -1)) return true;
+    }
+    
+    return false;
+}
